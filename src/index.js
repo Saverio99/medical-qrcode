10366748aaf385968c0a47469e92046687762282
Saverio Curcio <s.curcio@terraria.com>
2026-05-12 09:41:15 +0200

    add goahed after logging qrcode url

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ var QRCode = require('qrcode');
 async function generateQRCodeSVG(text){
     try {
         const url =  await QRCode.toDataURL(text);
-        console.log(url);
+        console.log(url + "\n");
         return url;   
     } catch (error) {
         console.log("Some error: ", error);
