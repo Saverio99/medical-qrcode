88b15049321de9170c2b5f7e3a65acf43830d5a7
Saverio Curcio <s.curcio@terraria.com>
2026-05-13 10:57:33 +0200

    add some documentation

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 var QRCode = require('qrcode');
 
+/**
+ * The function generate a QRCode SVG from some text
+ * @param {text} - The text you wnat to tranform in qrCode
+ * @return {utl} The QRCode SVG 
+ */
 async function generateQRCodeSVG(text){
     try {
-        const url =  await QRCode.toDataURL(text);
-        console.log(url + "\n");
-        return url;   
+        const svg =  await QRCode.toDataURL(text);
+        console.log(svg + "\n");
+        return svg;   
     } catch (error) {
         console.log("Some error: ", error);
         throw error;
     }
 }
 
+/**
+ * The function generate a QRCode string from some text
+ * @param {text} - The text you wnat to tranform in qrCode
+ * @return {utl} The QRCode url 
+ */
 async function generateQRCodeString(text){
     try {
         const url =  await QRCode.toString(text);
