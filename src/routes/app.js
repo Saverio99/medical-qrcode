48ce4377acdb0980a31a3296bc6f208478636fb5
Saverio Curcio <s.curcio@terraria.com>
2026-05-13 10:57:21 +0200

    update qrcode api to return an SVG

--- src/routes/app.js
+++ src/routes/app.js
@@ -1,15 +1,27 @@
-const express = require('express');
-const { generateQRCodeSVG } = require('..');
+const express = require("express");
+const { generateQRCodeSVG } = require("..");
 const app = express();
-const port = 8080;
+const port = 8090;
 
 app.use(express.json());
 
 // qrcode method is a Promise so we use async/await
-app.post('/qrcode', async (req, res) => {
-   const qrCode = await generateQRCodeSVG(req.body.text);
-   res.status(200).json({ message: `This is your qrcode: ${qrCode}` });
-})
+app.post("/qrcode", async (req, res) => {
+  try {
+    if (!req.body || Object.keys(req.body).length === 0) {
+      return res.status(400).json({ error: "Void body" });
+    }
+
+    const qrCode = await generateQRCodeSVG(JSON.stringify(req.body));
+
+    res.setHeader('Content-Type', 'image/svg+xml')
+    res.status(200).send(qrCode);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "We have encountered an error", error: error });
+  }
+});
 
 // start the server
 app.listen(port, () => {
