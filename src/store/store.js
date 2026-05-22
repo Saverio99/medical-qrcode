5898e09574e28d5769297354af59faaae862fbf4
Saverio Curcio <s.curcio@terraria.com>
2026-05-22 16:53:23 +0200

    modify store, now we store in DB (prisma)

--- src/store/store.js
+++ src/store/store.js
@@ -1,28 +1,38 @@
 const uuid = require('uuid');
 const { decrypt } = require('../crypt/cryptography');
-
-let map = new Map();
+const prisma = require('./prismaClient');
 
 /**
- * The method store encryped message into a map and return the corrispondent UUID 
+ * The method store encryped message into DB and return the corrispondent UUID 
  * @param {message} - The message you want to store in map
  * @return {UUID} The UUID of the message
  */
-function storeEncryptedMessage(message){
-    const id = uuid.v4() 
-    map.set(id, message);
-    return id;
+async function storeEncryptedMessage(message){
+    const profile = await prisma.medicalProfile.create({
+        data: {
+            data: message.content,
+            iv: message.iv,
+            tag: message.tag
+        }
+    });
+    return profile.id;
 }
 
 /**
- * The method return decrypted message by is UUID
+ * The method search and return decrypted message by is UUID from DB
  * @param {id} - The UUID we serach by
  * @return {decrypted} The decrypted message
  */
-function getEncryptedMessage(id){
-    const encryptedMessage = map.get(id);
-    const decryptedMessage = decrypt(encryptedMessage);
-    return decryptedMessage;
+async function getEncryptedMessage(id){
+        const profile = await prisma.medicalProfile.findUnique({
+        where: { id }
+    });
+    if (!profile) return null;
+    return {
+        content: profile.data,
+        iv: profile.iv,
+        tag: profile.tag
+    };
 }
 
 module.exports = {storeEncryptedMessage, getEncryptedMessage}
