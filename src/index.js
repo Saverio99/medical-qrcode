61587bbd98e9ab339a26b9b8cc2609ae908df65c
Saverio Curcio <s.curcio@terraria.com>
2026-05-26 16:09:31 +0200

    rename route

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cryptRoutes = require('./routes/crypt');
 const qrcodeRoutes = require('./routes/qrcode');
-const userRegistrationRoutes = require('./routes/userRegistration');
+const authRoutes = require('./routes/auth');
 const port = 8080;
 
 const app = express();
@@ -9,6 +9,6 @@ app.use(express.json());
 
 app.use('/crypt', cryptRoutes);
 app.use('/qrcode', qrcodeRoutes);
-app.use('/user', userRegistrationRoutes);
+app.use('/auth', authRoutes);
 
 app.listen(port, () => console.log('Server running on port 8080'));
